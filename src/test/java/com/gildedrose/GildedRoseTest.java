c843fb03864e3f1ce64fef1c0bfe23fcb4046225
daniel.r.steinberg <daniel.r.steinberg@CPX-GNIVY019GIV.dir.svc.accenture.com>
2017-11-07 10:53:39 -0600

    Split "foo" into two meaningful tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,21 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	public void itemHasSpecifiedType() {
+		Item[] items = new Item[] { new Item("foo", 0, 0) };
+		GildedRose app = new GildedRose(items);
+		assertEquals("foo", app.items[0].name);
+	}
+
+	@Test
+	public void typeRemainsUnchangedAtEndOfDay() {
+		Item[] items = new Item[] { new Item("foo", 0, 0) };
+		GildedRose app = new GildedRose(items);
+
+		app.updateQuality();
+
+		assertEquals("foo", app.items[0].name);
+	}
 
 }
