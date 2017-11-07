2a8185ef91ce17144721cd99d03c2c383dba7386
daniel.r.steinberg <daniel.r.steinberg@CPX-GNIVY019GIV.dir.svc.accenture.com>
2017-11-07 17:58:00 -0600

    Added test for default case

---
+++ src/test/java/com/gildedrose/ItemUpdaterFactoryTest.java
@@ -0,0 +1,21 @@
+package com.gildedrose;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemUpdaterFactoryTest {
+	private ItemUpdaterFactory itemUpdaterFactory;
+
+	@Before
+	public void setUp() {
+		itemUpdaterFactory = new ItemUpdaterFactory();
+	}
+
+	@Test
+	public void createsDefaultUpdaterForUnknownItem() throws Exception {
+		assertThat(itemUpdaterFactory.createItemUpdater("foo"), instanceOf(DefaultUpdater.class));
+	}
+}
