194d78fbcd576b7e8cf1e117fe5058e91a7dc239
daniel.r.steinberg <daniel.r.steinberg@CPX-GNIVY019GIV.dir.svc.accenture.com>
2017-11-07 16:42:08 -0600

    Restored original code (with loop). Now green.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,55 +11,54 @@ class GildedRose {
 	}
 
 	public void updateAtEndOfDay() {
-		// for (int i = 0; i < items.length; i++) {
-		int i = 0;
-		if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE_PASSES)) {
-			if (items[i].quality > 0) {
-				if (!items[i].name.equals(SULFURAS)) {
-					items[i].quality = items[i].quality - 1;
-				}
-			}
-		} else {
-			if (items[i].quality < 50) {
-				items[i].quality = items[i].quality + 1;
-
-				if (items[i].name.equals(BACKSTAGE_PASSES)) {
-					if (items[i].sellIn < 11) {
-						if (items[i].quality < 50) {
-							items[i].quality = items[i].quality + 1;
-						}
+		for (int i = 0; i < items.length; i++) {
+			if (!items[i].name.equals(AGED_BRIE) && !items[i].name.equals(BACKSTAGE_PASSES)) {
+				if (items[i].quality > 0) {
+					if (!items[i].name.equals(SULFURAS)) {
+						items[i].quality = items[i].quality - 1;
 					}
-
-					if (items[i].sellIn < 6) {
-						if (items[i].quality < 50) {
-							items[i].quality = items[i].quality + 1;
-						}
-					}
-				}
-			}
-		}
-
-		if (!items[i].name.equals(SULFURAS)) {
-			items[i].sellIn = items[i].sellIn - 1;
-		}
-
-		if (items[i].sellIn < 0) {
-			if (!items[i].name.equals(AGED_BRIE)) {
-				if (!items[i].name.equals(BACKSTAGE_PASSES)) {
-					if (items[i].quality > 0) {
-						if (!items[i].name.equals(SULFURAS)) {
-							items[i].quality = items[i].quality - 1;
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality;
 				}
 			} else {
 				if (items[i].quality < 50) {
 					items[i].quality = items[i].quality + 1;
+
+					if (items[i].name.equals(BACKSTAGE_PASSES)) {
+						if (items[i].sellIn < 11) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
+
+						if (items[i].sellIn < 6) {
+							if (items[i].quality < 50) {
+								items[i].quality = items[i].quality + 1;
+							}
+						}
+					}
+				}
+			}
+
+			if (!items[i].name.equals(SULFURAS)) {
+				items[i].sellIn = items[i].sellIn - 1;
+			}
+
+			if (items[i].sellIn < 0) {
+				if (!items[i].name.equals(AGED_BRIE)) {
+					if (!items[i].name.equals(BACKSTAGE_PASSES)) {
+						if (items[i].quality > 0) {
+							if (!items[i].name.equals(SULFURAS)) {
+								items[i].quality = items[i].quality - 1;
+							}
+						}
+					} else {
+						items[i].quality = items[i].quality - items[i].quality;
+					}
+				} else {
+					if (items[i].quality < 50) {
+						items[i].quality = items[i].quality + 1;
+					}
 				}
 			}
 		}
-		// }
 	}
 }
