5e400a7953c67af436554a7689d4908fb4f2b028
daniel.r.steinberg <daniel.r.steinberg@CPX-GNIVY019GIV.dir.svc.accenture.com>
2017-11-07 10:59:54 -0600

    Extracted createAppWithSingleItem

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,19 +8,25 @@ public class GildedRoseTest {
 
 	@Test
 	public void itemHasSpecifiedType() {
-		Item[] items = new Item[] { new Item("foo", 0, 0) };
-		GildedRose app = new GildedRose(items);
+		GildedRose app = createAppWithSingleItem("foo", 0, 0);
 		assertEquals("foo", app.items[0].name);
 	}
 
 	@Test
 	public void typeRemainsUnchangedAtEndOfDay() {
-		Item[] items = new Item[] { new Item("foo", 0, 0) };
-		GildedRose app = new GildedRose(items);
+		GildedRose app = createAppWithSingleItem("foo", 0, 0);
 
 		app.updateQuality();
 
 		assertEquals("foo", app.items[0].name);
+	}
+
+	private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
+		return new GildedRose(createSingleItemArray(name, sellIn, quality));
+	}
+
+	private Item[] createSingleItemArray(String name, int sellIn, int quality) {
+		return new Item[] { new Item(name, sellIn, quality) };
 	}
 
 }
