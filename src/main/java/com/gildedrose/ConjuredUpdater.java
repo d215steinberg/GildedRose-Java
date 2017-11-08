cc21883f39d19f4cfb8375a3b85544ca14bcfd74
daniel.r.steinberg <daniel.r.steinberg@CPX-GNIVY019GIV.dir.svc.accenture.com>
2017-11-07 18:36:45 -0600

    Extracted decreaseQuality

--- src/main/java/com/gildedrose/ConjuredUpdater.java
+++ src/main/java/com/gildedrose/ConjuredUpdater.java
@@ -6,7 +6,11 @@ public class ConjuredUpdater implements ItemUpdater {
 
 	@Override
 	public void updateQuality(Item item) {
-		item.quality = max(item.quality - getQualityDecrement(item.sellIn), 0);
+		item.quality = decreaseQuality(item.quality, item.sellIn);
+	}
+
+	private int decreaseQuality(int quality, int sellIn) {
+		return max(quality - getQualityDecrement(sellIn), 0);
 	}
 
 	private int getQualityDecrement(int sellIn) {
