668b5c0da6c17259815511ab92ffa1e0d6b529a6
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2019-11-05 17:39:20 -0600

    Merge from Lesson#7

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -64,7 +64,7 @@ public class GildedRoseTest {
 	}
 
 	private Item getLoneItem() {
-		assert app.items.length == 1 : "More than one item";
+		assert app.items.length == 1 : "Expecting exactly one item";
 		return getFirstItem();
 	}
 
