e4acb98ee025052069abe9967fe3ea01d35e41c0
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2019-11-06 14:25:03 -0600

    Implemented forName using Java8 streams/lambdas

--- src/main/java/com/gildedrose/ItemType.java
+++ src/main/java/com/gildedrose/ItemType.java
@@ -1,8 +1,10 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 public enum ItemType {
-	AGED_BRIE("Aged Brie"), SULFURAS("Sulfuras, Hand of Ragnaros"), BACKSTAGE_PASSES("Backstage passes to a TAFKAL80ETC concert"),
-	CONJURED("Conjured Mana Cake") {
+	AGED_BRIE("Aged Brie"), SULFURAS("Sulfuras, Hand of Ragnaros"),
+	BACKSTAGE_PASSES("Backstage passes to a TAFKAL80ETC concert"), CONJURED("Conjured Mana Cake") {
 		@Override
 		public ItemUpdater createItemUpdater() {
 			return new ConjuredUpdater();
@@ -22,12 +24,8 @@ public enum ItemType {
 	}
 
 	public static ItemType forName(String name) {
-		for (ItemType itemType : ItemType.values()) {
-			if (name.equals(itemType.name)) {
-				return itemType;
-			}
-		}
-		return UNKNOWN;
+		return Arrays.stream(ItemType.values()).filter(itemType -> name.equals(itemType.name)).findFirst()
+				.orElse(ItemType.UNKNOWN);
 	}
 
 	public ItemUpdater createItemUpdater() {
