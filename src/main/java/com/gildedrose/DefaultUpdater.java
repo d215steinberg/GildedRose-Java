0018d6cc1e7b06d7ca91cfe89df82bb1e9744f15
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2019-11-06 15:03:35 -0600

    Removed dead Aged Brie code from DefaultUpdater

--- src/main/java/com/gildedrose/DefaultUpdater.java
+++ src/main/java/com/gildedrose/DefaultUpdater.java
@@ -1,13 +1,12 @@
 package com.gildedrose;
 
-import static com.gildedrose.ItemType.AGED_BRIE;
 import static com.gildedrose.ItemType.BACKSTAGE_PASSES;
 import static com.gildedrose.ItemType.SULFURAS;
 
 public class DefaultUpdater implements ItemUpdater {
 	@Override
 	public void updateQuality(Item item) {
-		if (!item.name.equals(AGED_BRIE.name) && !item.name.equals(BACKSTAGE_PASSES.name)) {
+		if (!item.name.equals(BACKSTAGE_PASSES.name)) {
 			if (item.quality > 0) {
 				if (!item.name.equals(SULFURAS.name)) {
 					item.quality = item.quality - 1;
@@ -34,20 +33,14 @@ public class DefaultUpdater implements ItemUpdater {
 		}
 
 		if (item.sellIn <= 0) {
-			if (!item.name.equals(AGED_BRIE.name)) {
-				if (!item.name.equals(BACKSTAGE_PASSES.name)) {
-					if (item.quality > 0) {
-						if (!item.name.equals(SULFURAS.name)) {
-							item.quality = item.quality - 1;
-						}
+			if (!item.name.equals(BACKSTAGE_PASSES.name)) {
+				if (item.quality > 0) {
+					if (!item.name.equals(SULFURAS.name)) {
+						item.quality = item.quality - 1;
 					}
-				} else {
-					item.quality = 0;
 				}
 			} else {
-				if (item.quality < MAX_QUALITY) {
-					item.quality = item.quality + 1;
-				}
+				item.quality = 0;
 			}
 		}
 	}
