c07fac10b658283799165fe3bdb53e93a67248bb
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2019-11-07 11:30:21 -0600

    Created BackstagePassesUpdater
    Extracted QualityIncreaser with subclasses
    Extracted ExpirationChecker

--- src/main/java/com/gildedrose/DefaultUpdater.java
+++ src/main/java/com/gildedrose/DefaultUpdater.java
@@ -1,41 +1,14 @@
 package com.gildedrose;
 
-import static com.gildedrose.ItemType.BACKSTAGE_PASSES;
-
 public class DefaultUpdater implements ItemUpdater {
 	@Override
 	public void updateQuality(Item item) {
-		if (!item.name.equals(BACKSTAGE_PASSES.name)) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+		if (item.sellIn <= 0) {
 			if (item.quality > 0) {
 				item.quality = item.quality - 1;
-			}
-		} else {
-			if (item.quality < MAX_QUALITY) {
-				item.quality = item.quality + 1;
-
-				if (item.name.equals(BACKSTAGE_PASSES.name)) {
-					if (item.sellIn < 11) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = item.quality + 1;
-						}
-					}
-
-					if (item.sellIn < 6) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = item.quality + 1;
-						}
-					}
-				}
-			}
-		}
-
-		if (item.sellIn <= 0) {
-			if (!item.name.equals(BACKSTAGE_PASSES.name)) {
-				if (item.quality > 0) {
-					item.quality = item.quality - 1;
-				}
-			} else {
-				item.quality = 0;
 			}
 		}
 	}
@@ -43,9 +16,5 @@ public class DefaultUpdater implements ItemUpdater {
 	@Override
 	public void updateSellIn(Item item) {
 		item.sellIn = item.sellIn - 1;
-	}
-
-	protected boolean sellDateHasPassed(int sellIn) {
-		return sellIn <= 0;
 	}
 }
