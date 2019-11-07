72cf8f807bdefa1e84e91a03d079caf94f1cb624
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2019-11-07 13:59:26 -0600

    Rolled back removal of Backstage Passes code from DefaultUpdater (deferred to Lesson#37)

--- src/main/java/com/gildedrose/DefaultUpdater.java
+++ src/main/java/com/gildedrose/DefaultUpdater.java
@@ -1,14 +1,42 @@
 package com.gildedrose;
 
+import static com.gildedrose.ItemType.BACKSTAGE_PASSES;
+import static com.gildedrose.QualityIncreaser.MAX_QUALITY;
+
 public class DefaultUpdater implements ItemUpdater {
 	@Override
 	public void updateQuality(Item item) {
-		if (item.quality > 0) {
-			item.quality = item.quality - 1;
-		}
-		if (item.sellIn <= 0) {
+		if (!item.name.equals(BACKSTAGE_PASSES.name)) {
 			if (item.quality > 0) {
 				item.quality = item.quality - 1;
+			}
+		} else {
+			if (item.quality < MAX_QUALITY) {
+				item.quality = item.quality + 1;
+
+				if (item.name.equals(BACKSTAGE_PASSES.name)) {
+					if (item.sellIn < 11) {
+						if (item.quality < MAX_QUALITY) {
+							item.quality = item.quality + 1;
+						}
+					}
+
+					if (item.sellIn < 6) {
+						if (item.quality < MAX_QUALITY) {
+							item.quality = item.quality + 1;
+						}
+					}
+				}
+			}
+		}
+
+		if (item.sellIn <= 0) {
+			if (!item.name.equals(BACKSTAGE_PASSES.name)) {
+				if (item.quality > 0) {
+					item.quality = item.quality - 1;
+				}
+			} else {
+				item.quality = 0;
 			}
 		}
 	}
@@ -17,4 +45,5 @@ public class DefaultUpdater implements ItemUpdater {
 	public void updateSellIn(Item item) {
 		item.sellIn = item.sellIn - 1;
 	}
+
 }
