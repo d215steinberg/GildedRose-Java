e406ab7be37548f4cf2bb4844b01411c8b5e842a
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2019-11-07 14:06:06 -0600

    Removed Backstage Passes logic from DefaultUpdater

--- src/main/java/com/gildedrose/DefaultUpdater.java
+++ src/main/java/com/gildedrose/DefaultUpdater.java
@@ -1,42 +1,15 @@
 package com.gildedrose;
 
-import static com.gildedrose.ItemType.BACKSTAGE_PASSES;
-import static com.gildedrose.QualityIncreaser.MAX_QUALITY;
-
 public class DefaultUpdater implements ItemUpdater {
 	@Override
 	public void updateQuality(Item item) {
-		if (!item.name.equals(BACKSTAGE_PASSES.name)) {
-			if (item.quality > 0) {
-				item.quality = item.quality - 1;
-			}
-		} else {
-			if (item.quality < MAX_QUALITY) {
-				item.quality = item.quality + 1;
-
-				if (item.name.equals(BACKSTAGE_PASSES.name)) {
-					if (item.sellIn < 11) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = item.quality + 1;
-						}
-					}
-
-					if (item.sellIn < 6) {
-						if (item.quality < MAX_QUALITY) {
-							item.quality = item.quality + 1;
-						}
-					}
-				}
-			}
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
 		}
 
 		if (item.sellIn <= 0) {
-			if (!item.name.equals(BACKSTAGE_PASSES.name)) {
-				if (item.quality > 0) {
-					item.quality = item.quality - 1;
-				}
-			} else {
-				item.quality = 0;
+			if (item.quality > 0) {
+				item.quality = item.quality - 1;
 			}
 		}
 	}
