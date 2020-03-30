83d6eba4299466c99d353cb33d41aa6e49d7a6f1
Steinberg, Daniel R. <daniel.r.steinberg@accenture.com>
2020-03-30 15:33:54 -0400

    Changed test names to refer to *name* instead of *type*

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 public class GildedRoseTest {
 
 	@Test
-	public void itemHasSpecifiedType() {
+	public void itemHasSpecifiedName() {
 		Item[] items = new Item[] { new Item("foo", 0, 0) };
 		GildedRose app = new GildedRose(items);
 		assertEquals("foo", app.items[0].name);
 	}
 
 	@Test
-	public void typeRemainsUnchangedAtEndOfDay() {
+	public void nameRemainsUnchangedAtEndOfDay() {
 		Item[] items = new Item[] { new Item("foo", 0, 0) };
 		GildedRose app = new GildedRose(items);
 
