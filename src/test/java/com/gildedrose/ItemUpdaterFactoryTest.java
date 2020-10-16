3326dca3dad1b0cb90b1561b35f467512cb37e78
Steinberg, Daniel R <daniel.r.steinberg@accenture.com>
2020-10-16 13:18:45 -0400

    Import assertThat from Hamcrest instead of jUnit in ItemUpdaterFactoryTest

--- src/test/java/com/gildedrose/ItemUpdaterFactoryTest.java
+++ src/test/java/com/gildedrose/ItemUpdaterFactoryTest.java
@@ -1,7 +1,7 @@
 package com.gildedrose;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
