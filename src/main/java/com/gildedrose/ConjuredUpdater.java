eaa1c4ae56a5c4416ed842617e7e624758edc12e
daniel.r.steinberg <daniel.r.steinberg@CPX-MOMN87550JV.dir.svc.accenture.com>
2021-03-02 11:47:51 -0500

    Removed empty implementation of updateQuality

--- src/main/java/com/gildedrose/ConjuredUpdater.java
+++ src/main/java/com/gildedrose/ConjuredUpdater.java
@@ -2,10 +2,4 @@ package com.gildedrose;
 
 public class ConjuredUpdater extends DefaultUpdater {
 
-	@Override
-	public void updateQuality(Item item) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
