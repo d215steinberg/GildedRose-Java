4e15136bfe93aee6d34e9a19449030f847c87d6e
Daniel Steinberg <d215steinberg@yahoo.com>
2021-11-08 13:15:42 -0500

    Tabs to spaces

--- src/test/java/com/gildedrose/GildedRoseConjuredTest.java
+++ src/test/java/com/gildedrose/GildedRoseConjuredTest.java
@@ -7,32 +7,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
 
 public class GildedRoseConjuredTest extends GildedRoseTest {
-	@Test
-	public void conjuredQualityDecreasesBy2() {
-		app = createAppWithSingleItem(CONJURED.name, ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 2));
-	}
+    @Test
+    public void conjuredQualityDecreasesBy2() {
+        app = createAppWithSingleItem(CONJURED.name, ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 2));
+    }
 
-	@Test
-	public void conjuredQualityDecreasesBy4OnceSellDateHasPassed() {
-		app = createAppWithSingleItem(CONJURED.name, 0, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 4));
-	}
+    @Test
+    public void conjuredQualityDecreasesBy4OnceSellDateHasPassed() {
+        app = createAppWithSingleItem(CONJURED.name, 0, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 4));
+    }
 
-	@Test
-	public void conjuredQualityIsNeverNegative() {
-		app = createAppWithSingleItem(CONJURED.name, ARBITRARY_SELLIN, 1);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(0));
-	}
+    @Test
+    public void conjuredQualityIsNeverNegative() {
+        app = createAppWithSingleItem(CONJURED.name, ARBITRARY_SELLIN, 1);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(0));
+    }
 
-	@Test
-	public void conjuredQualityIsNeverNegativeEvenOnceSellDateHasPassed() {
-		app = createAppWithSingleItem(CONJURED.name, 0, 3);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(0));
-	}
+    @Test
+    public void conjuredQualityIsNeverNegativeEvenOnceSellDateHasPassed() {
+        app = createAppWithSingleItem(CONJURED.name, 0, 3);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(0));
+    }
 
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,24 +2,24 @@ package com.gildedrose;
 
 public class GildedRoseTest {
 
-	protected static final int ARBITRARY_SELLIN = 17;
-	protected static final int ARBITRARY_QUALITY = 19;
-	protected GildedRose app;
+    protected static final int ARBITRARY_SELLIN = 17;
+    protected static final int ARBITRARY_QUALITY = 19;
+    protected GildedRose app;
 
-	protected GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
-		return new GildedRose(createSingleItemArray(name, sellIn, quality));
-	}
+    protected GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
+        return new GildedRose(createSingleItemArray(name, sellIn, quality));
+    }
 
-	private Item[] createSingleItemArray(String name, int sellIn, int quality) {
-		return new Item[] { new Item(name, sellIn, quality) };
-	}
+    private Item[] createSingleItemArray(String name, int sellIn, int quality) {
+        return new Item[]{new Item(name, sellIn, quality)};
+    }
 
-	protected Item getLoneItem() {
-		assert app.items.length == 1 : "Expecting exactly one item";
-		return getFirstItem();
-	}
+    protected Item getLoneItem() {
+        assert app.items.length == 1 : "Expecting exactly one item";
+        return getFirstItem();
+    }
 
-	private Item getFirstItem() {
-		return app.items[0];
-	}
+    private Item getFirstItem() {
+        return app.items[0];
+    }
 }
