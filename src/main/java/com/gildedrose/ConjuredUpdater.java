d48d0a1af007429b33a49e27aa6e5b4d625712ee
Daniel Steinberg <d215steinberg@yahoo.com>
2021-11-08 11:37:45 -0500

    Added @Override annotation to **getQualityDecrement**

--- src/main/java/com/gildedrose/ConjuredUpdater.java
+++ src/main/java/com/gildedrose/ConjuredUpdater.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 public class ConjuredUpdater extends DefaultUpdater {
 
+	@Override
 	protected int getQualityDecrement(int sellIn) {
 		return sellDateHasPassed(sellIn) ? 4 : 2;
 	}
