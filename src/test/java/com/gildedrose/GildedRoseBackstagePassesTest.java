02922a853e13cafca3ae0a4c1d349efaada38d2b
Daniel Steinberg <d215steinberg@yahoo.com>
2021-11-08 13:52:46 -0500

    Tabs to spaces

--- src/test/java/com/gildedrose/GildedRoseBackstagePassesTest.java
+++ src/test/java/com/gildedrose/GildedRoseBackstagePassesTest.java
@@ -10,60 +10,60 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
 
 public class GildedRoseBackstagePassesTest extends GildedRoseTest {
-	@Test
-	public void backstagePassesQualityIncreasesBy1BeyondDoubleAppreciationThreshold() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, DOUBLE_APPRECIATION_THRESHOLD + 1, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 1));
-	}
+    @Test
+    public void backstagePassesQualityIncreasesBy1BeyondDoubleAppreciationThreshold() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, DOUBLE_APPRECIATION_THRESHOLD + 1, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 1));
+    }
 
-	@Test
-	public void backstagePassesQualityIncreasesBy2WithinDoubleAppreciationThreshold() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, DOUBLE_APPRECIATION_THRESHOLD, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 2));
-	}
+    @Test
+    public void backstagePassesQualityIncreasesBy2WithinDoubleAppreciationThreshold() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, DOUBLE_APPRECIATION_THRESHOLD, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 2));
+    }
 
-	@Test
-	public void backstagePassesQualityIncreasesBy2BeyondTripleAppreciationThreshold() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, TRIPLE_APPRECIATION_THRESHOLD + 1, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 2));
-	}
+    @Test
+    public void backstagePassesQualityIncreasesBy2BeyondTripleAppreciationThreshold() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, TRIPLE_APPRECIATION_THRESHOLD + 1, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 2));
+    }
 
-	@Test
-	public void backstagePassesQualityDoesNotExceedMaximumWithinDoubleAppreciationThreshold() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, DOUBLE_APPRECIATION_THRESHOLD, MAX_QUALITY - 1);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(MAX_QUALITY));
-	}
+    @Test
+    public void backstagePassesQualityDoesNotExceedMaximumWithinDoubleAppreciationThreshold() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, DOUBLE_APPRECIATION_THRESHOLD, MAX_QUALITY - 1);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(MAX_QUALITY));
+    }
 
-	@Test
-	public void backstagePassesQualityIncreasesBy3WithinTripleAppreciationThreshold() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, TRIPLE_APPRECIATION_THRESHOLD, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 3));
-	}
+    @Test
+    public void backstagePassesQualityIncreasesBy3WithinTripleAppreciationThreshold() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, TRIPLE_APPRECIATION_THRESHOLD, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 3));
+    }
 
-	@Test
-	public void backstagePassesQualityIncreasesBy3UpToConcertDate() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, 1, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 3));
-	}
+    @Test
+    public void backstagePassesQualityIncreasesBy3UpToConcertDate() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, 1, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY + 3));
+    }
 
-	@Test
-	public void backstagePassesQualityDoesNotExceedMaximumWithinTripleAppreciationThreshold() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, TRIPLE_APPRECIATION_THRESHOLD, MAX_QUALITY - 2);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(MAX_QUALITY));
-	}
+    @Test
+    public void backstagePassesQualityDoesNotExceedMaximumWithinTripleAppreciationThreshold() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, TRIPLE_APPRECIATION_THRESHOLD, MAX_QUALITY - 2);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(MAX_QUALITY));
+    }
 
-	@Test
-	public void backstagePassesQualityDropsToZeroOnceConcertHasPassed() {
-		app = createAppWithSingleItem(BACKSTAGE_PASSES.name, 0, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(0));
-	}
+    @Test
+    public void backstagePassesQualityDropsToZeroOnceConcertHasPassed() {
+        app = createAppWithSingleItem(BACKSTAGE_PASSES.name, 0, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(0));
+    }
 
 }
--- src/test/java/com/gildedrose/GildedRoseDefaultTest.java
+++ src/test/java/com/gildedrose/GildedRoseDefaultTest.java
@@ -6,64 +6,64 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
 
 public class GildedRoseDefaultTest extends GildedRoseTest {
-	@Test
-	public void itemHasSpecifiedName() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		assertThat(getLoneItem().name, is("foo"));
-	}
+    @Test
+    public void itemHasSpecifiedName() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        assertThat(getLoneItem().name, is("foo"));
+    }
 
-	@Test
-	public void itemHasSpecifiedSellIn() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		assertThat(getLoneItem().sellIn, is(ARBITRARY_SELLIN));
-	}
+    @Test
+    public void itemHasSpecifiedSellIn() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        assertThat(getLoneItem().sellIn, is(ARBITRARY_SELLIN));
+    }
 
-	@Test
-	public void itemHasSpecifiedQuality() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY));
-	}
+    @Test
+    public void itemHasSpecifiedQuality() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY));
+    }
 
-	@Test
-	public void nameRemainsUnchangedAtEndOfDay() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().name, is("foo"));
-	}
+    @Test
+    public void nameRemainsUnchangedAtEndOfDay() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().name, is("foo"));
+    }
 
-	@Test
-	public void sellInDecreasesAtEndOfDay() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().sellIn, is(ARBITRARY_SELLIN - 1));
-	}
+    @Test
+    public void sellInDecreasesAtEndOfDay() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().sellIn, is(ARBITRARY_SELLIN - 1));
+    }
 
-	@Test
-	public void qualityDecreasesBy1AtEndOfDay() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 1));
-	}
+    @Test
+    public void qualityDecreasesBy1AtEndOfDay() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 1));
+    }
 
-	@Test
-	public void qualityDecreasesBy2AtEndOfDayOnceSellDateHasPassed() {
-		app = createAppWithSingleItem("foo", 0, ARBITRARY_QUALITY);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 2));
-	}
+    @Test
+    public void qualityDecreasesBy2AtEndOfDayOnceSellDateHasPassed() {
+        app = createAppWithSingleItem("foo", 0, ARBITRARY_QUALITY);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(ARBITRARY_QUALITY - 2));
+    }
 
-	@Test
-	public void qualityIsNeverNegative() {
-		app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, 0);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(0));
-	}
+    @Test
+    public void qualityIsNeverNegative() {
+        app = createAppWithSingleItem("foo", ARBITRARY_SELLIN, 0);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(0));
+    }
 
-	@Test
-	public void qualityIsNeverNegativeEvenOnceSellDateHasPassed() {
-		app = createAppWithSingleItem("foo", 0, 1);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(0));
-	}
+    @Test
+    public void qualityIsNeverNegativeEvenOnceSellDateHasPassed() {
+        app = createAppWithSingleItem("foo", 0, 1);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(0));
+    }
 
 }
