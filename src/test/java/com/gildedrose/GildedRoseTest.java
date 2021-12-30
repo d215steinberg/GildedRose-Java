20eb775465fb85010e7a4427252b2e78927b3e27
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 12:38:29 -0500

    Tabs to spaces

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,25 +6,25 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-	@Test
-	public void itemHasSpecifiedName() {
-		GildedRose app = createAppWithSingleItem("foo", 0, 0);
-		assertEquals("foo", app.items[0].name);
-	}
+    @Test
+    public void itemHasSpecifiedName() {
+        GildedRose app = createAppWithSingleItem("foo", 0, 0);
+        assertEquals("foo", app.items[0].name);
+    }
 
-	@Test
-	public void nameRemainsUnchangedAtEndOfDay() {
-		GildedRose app = createAppWithSingleItem("foo", 0, 0);
-		app.updateQuality();
-		assertEquals("foo", app.items[0].name);
-	}
+    @Test
+    public void nameRemainsUnchangedAtEndOfDay() {
+        GildedRose app = createAppWithSingleItem("foo", 0, 0);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
 
-	private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
-		return new GildedRose(createSingleItemArray(name, sellIn, quality));
-	}
+    private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
+        return new GildedRose(createSingleItemArray(name, sellIn, quality));
+    }
 
-	private Item[] createSingleItemArray(String name, int sellIn, int quality) {
-		return new Item[] { new Item(name, sellIn, quality) };
-	}
+    private Item[] createSingleItemArray(String name, int sellIn, int quality) {
+        return new Item[]{new Item(name, sellIn, quality)};
+    }
 
 }
