f0405d3c3ab336697372d8f1e56a2c321c2c2ef8
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 14:44:37 -0500

    Tabs to spaces

--- src/main/java/com/gildedrose/DefaultUpdater.java
+++ src/main/java/com/gildedrose/DefaultUpdater.java
@@ -5,57 +5,57 @@ import static com.gildedrose.ItemType.BACKSTAGE_PASSES;
 import static com.gildedrose.ItemType.SULFURAS;
 
 public class DefaultUpdater implements ItemUpdater {
-	@Override
-	public void updateQuality(Item item) {
-		if (!item.name.equals(AGED_BRIE.name) && !item.name.equals(BACKSTAGE_PASSES.name)) {
-			if (item.quality > 0) {
-				if (!item.name.equals(SULFURAS.name)) {
-					item.quality = item.quality - 1;
-				}
-			}
-		} else {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
+    @Override
+    public void updateQuality(Item item) {
+        if (!item.name.equals(AGED_BRIE.name) && !item.name.equals(BACKSTAGE_PASSES.name)) {
+            if (item.quality > 0) {
+                if (!item.name.equals(SULFURAS.name)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-				if (item.name.equals(BACKSTAGE_PASSES.name)) {
-					if (item.sellIn < 11) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
-					}
+                if (item.name.equals(BACKSTAGE_PASSES.name)) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
 
-					if (item.sellIn < 6) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
-					}
-				}
-			}
-		}
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
 
-		if (item.sellIn <= 0) {
-			if (!item.name.equals(AGED_BRIE.name)) {
-				if (!item.name.equals(BACKSTAGE_PASSES.name)) {
-					if (item.quality > 0) {
-						if (!item.name.equals(SULFURAS.name)) {
-							item.quality = item.quality - 1;
-						}
-					}
-				} else {
-					item.quality = 0;
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-			}
-		}
-	}
+        if (item.sellIn <= 0) {
+            if (!item.name.equals(AGED_BRIE.name)) {
+                if (!item.name.equals(BACKSTAGE_PASSES.name)) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS.name)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
 
-	@Override
-	public void updateSellIn(Item item) {
-		if (!item.name.equals(SULFURAS.name)) {
-			item.sellIn = item.sellIn - 1;
-		}
-	}
+    @Override
+    public void updateSellIn(Item item) {
+        if (!item.name.equals(SULFURAS.name)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
 }
