9d5fca9b7888fdf11b3d198bfe5b4695b6290bf0
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 12:45:28 -0500

    Tabs to spaces

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,68 +7,68 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-	private GildedRose app;
+    private GildedRose app;
 
-	@Test
-	public void itemHasSpecifiedName() {
-		app = createAppWithSingleItem("foo", 0, 0);
-		assertThat(getLoneItem().name, is("foo"));
-	}
+    @Test
+    public void itemHasSpecifiedName() {
+        app = createAppWithSingleItem("foo", 0, 0);
+        assertThat(getLoneItem().name, is("foo"));
+    }
 
-	@Test
-	public void itemHasSpecifiedSellIn() {
-		app = createAppWithSingleItem("foo", 17, 0);
-		assertThat(getLoneItem().sellIn, is(17));
-	}
+    @Test
+    public void itemHasSpecifiedSellIn() {
+        app = createAppWithSingleItem("foo", 17, 0);
+        assertThat(getLoneItem().sellIn, is(17));
+    }
 
-	@Test
-	public void itemHasSpecifiedQuality() {
-		app = createAppWithSingleItem("foo", 0, 19);
-		assertThat(getLoneItem().quality, is(19));
-	}
+    @Test
+    public void itemHasSpecifiedQuality() {
+        app = createAppWithSingleItem("foo", 0, 19);
+        assertThat(getLoneItem().quality, is(19));
+    }
 
-	@Test
-	public void nameRemainsUnchangedAtEndOfDay() {
-		app = createAppWithSingleItem("foo", 0, 0);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().name, is("foo"));
-	}
+    @Test
+    public void nameRemainsUnchangedAtEndOfDay() {
+        app = createAppWithSingleItem("foo", 0, 0);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().name, is("foo"));
+    }
 
-	@Test
-	public void sellInDecreasesAtEndOfDay() {
-		app = createAppWithSingleItem("foo", 17, 0);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().sellIn, is(16));
-	}
+    @Test
+    public void sellInDecreasesAtEndOfDay() {
+        app = createAppWithSingleItem("foo", 17, 0);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().sellIn, is(16));
+    }
 
-	@Test
-	public void qualityDecreasesBy1AtEndOfDay() {
-		app = createAppWithSingleItem("foo", 17, 19);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(18));
-	}
+    @Test
+    public void qualityDecreasesBy1AtEndOfDay() {
+        app = createAppWithSingleItem("foo", 17, 19);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(18));
+    }
 
-	@Test
-	public void qualityDecreasesBy2AtEndOfDayOnceSellDateHasPassed() {
-		app = createAppWithSingleItem("foo", 0, 19);
-		app.updateAtEndOfDay();
-		assertThat(getLoneItem().quality, is(17));
-	}
+    @Test
+    public void qualityDecreasesBy2AtEndOfDayOnceSellDateHasPassed() {
+        app = createAppWithSingleItem("foo", 0, 19);
+        app.updateAtEndOfDay();
+        assertThat(getLoneItem().quality, is(17));
+    }
 
-	private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
-		return new GildedRose(createSingleItemArray(name, sellIn, quality));
-	}
+    private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
+        return new GildedRose(createSingleItemArray(name, sellIn, quality));
+    }
 
-	private Item[] createSingleItemArray(String name, int sellIn, int quality) {
-		return new Item[] { new Item(name, sellIn, quality) };
-	}
+    private Item[] createSingleItemArray(String name, int sellIn, int quality) {
+        return new Item[]{new Item(name, sellIn, quality)};
+    }
 
-	private Item getLoneItem() {
-		assert app.items.length == 1 : "Expecting exactly one item";
-		return getFirstItem();
-	}
+    private Item getLoneItem() {
+        assert app.items.length == 1 : "Expecting exactly one item";
+        return getFirstItem();
+    }
 
-	private Item getFirstItem() {
-		return app.items[0];
-	}
+    private Item getFirstItem() {
+        return app.items[0];
+    }
 }
