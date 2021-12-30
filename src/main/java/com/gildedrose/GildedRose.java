5d989311bbaea1868b2d34608f6c3d02262ae74a
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 14:37:46 -0500

    Tabs to spaces

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,26 +1,26 @@
 package com.gildedrose;
 
 class GildedRose {
-	static final String AGED_BRIE = "Aged Brie";
-	static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-	static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-	static final String CONJURED = "Conjured Mana Cake";
-	Item[] items;
-	private ItemUpdaterFactory itemUpdaterFactory = new ItemUpdaterFactory();
+    static final String AGED_BRIE = "Aged Brie";
+    static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    static final String CONJURED = "Conjured Mana Cake";
+    Item[] items;
+    private ItemUpdaterFactory itemUpdaterFactory = new ItemUpdaterFactory();
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
 
-	public void updateAtEndOfDay() {
-		for (Item item : items) {
-			ItemUpdater itemUpdater = createItemUpdater(item.name);
-			itemUpdater.updateQuality(item);
-			itemUpdater.updateSellIn(item);
-		}
-	}
+    public void updateAtEndOfDay() {
+        for (Item item : items) {
+            ItemUpdater itemUpdater = createItemUpdater(item.name);
+            itemUpdater.updateQuality(item);
+            itemUpdater.updateSellIn(item);
+        }
+    }
 
-	private ItemUpdater createItemUpdater(String itemName) {
-		return itemUpdaterFactory.createItemUpdater(itemName);
-	}
+    private ItemUpdater createItemUpdater(String itemName) {
+        return itemUpdaterFactory.createItemUpdater(itemName);
+    }
 }
