b8f3618cfc777c73db50a4a5e84c4865ae19a6ca
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 12:41:10 -0500

    Tabs to spaces

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,35 +6,35 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-	private GildedRose app;
+    private GildedRose app;
 
-	@Test
-	public void itemHasSpecifiedName() {
-		app = createAppWithSingleItem("foo", 0, 0);
-		assertEquals("foo", getLoneItem().name);
-	}
+    @Test
+    public void itemHasSpecifiedName() {
+        app = createAppWithSingleItem("foo", 0, 0);
+        assertEquals("foo", getLoneItem().name);
+    }
 
-	@Test
-	public void nameRemainsUnchangedAtEndOfDay() {
-		app = createAppWithSingleItem("foo", 0, 0);
-		app.updateAtEndOfDay();
-		assertEquals("foo", getLoneItem().name);
-	}
+    @Test
+    public void nameRemainsUnchangedAtEndOfDay() {
+        app = createAppWithSingleItem("foo", 0, 0);
+        app.updateAtEndOfDay();
+        assertEquals("foo", getLoneItem().name);
+    }
 
-	private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
-		return new GildedRose(createSingleItemArray(name, sellIn, quality));
-	}
+    private GildedRose createAppWithSingleItem(String name, int sellIn, int quality) {
+        return new GildedRose(createSingleItemArray(name, sellIn, quality));
+    }
 
-	private Item[] createSingleItemArray(String name, int sellIn, int quality) {
-		return new Item[] { new Item(name, sellIn, quality) };
-	}
+    private Item[] createSingleItemArray(String name, int sellIn, int quality) {
+        return new Item[]{new Item(name, sellIn, quality)};
+    }
 
-	private Item getLoneItem() {
-		assert app.items.length == 1 : "Expecting exactly one item";
-		return getFirstItem();
-	}
+    private Item getLoneItem() {
+        assert app.items.length == 1 : "Expecting exactly one item";
+        return getFirstItem();
+    }
 
-	private Item getFirstItem() {
-		return app.items[0];
-	}
+    private Item getFirstItem() {
+        return app.items[0];
+    }
 }
