db1df73a5bbe7afd1822d272d0675311e4243410
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 14:27:21 -0500

    Tabs to spaces

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -3,12 +3,12 @@ package com.gildedrose;
 import static com.gildedrose.GildedRose.CONJURED;
 
 public class ItemUpdaterFactory {
-	public ItemUpdater createItemUpdater(String itemName) {
-		switch (itemName) {
-		case CONJURED:
-			return new ConjuredUpdater();
-		default:
-			return new DefaultUpdater();
-		}
-	}
+    public ItemUpdater createItemUpdater(String itemName) {
+        switch (itemName) {
+            case CONJURED:
+                return new ConjuredUpdater();
+            default:
+                return new DefaultUpdater();
+        }
+    }
 }
