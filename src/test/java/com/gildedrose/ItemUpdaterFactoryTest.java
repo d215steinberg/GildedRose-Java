24055fb2e70c34d9c5a2fbabe1cfc048a40c590c
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 14:25:37 -0500

    Tabs to spaces

--- src/test/java/com/gildedrose/ItemUpdaterFactoryTest.java
+++ src/test/java/com/gildedrose/ItemUpdaterFactoryTest.java
@@ -7,15 +7,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ItemUpdaterFactoryTest {
-	private ItemUpdaterFactory itemUpdaterFactory;
+    private ItemUpdaterFactory itemUpdaterFactory;
 
-	@Before
-	public void setUp() {
-		itemUpdaterFactory = new ItemUpdaterFactory();
-	}
+    @Before
+    public void setUp() {
+        itemUpdaterFactory = new ItemUpdaterFactory();
+    }
 
-	@Test
-	public void createsDefaultUpdaterForUnknownItem() throws Exception {
-		assertThat(itemUpdaterFactory.createItemUpdater("foo"), instanceOf(DefaultUpdater.class));
-	}
+    @Test
+    public void createsDefaultUpdaterForUnknownItem() throws Exception {
+        assertThat(itemUpdaterFactory.createItemUpdater("foo"), instanceOf(DefaultUpdater.class));
+    }
 }
