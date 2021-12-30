796b9e1dbfce0a87fc7e191c1f098f1d6e2975f7
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 14:23:23 -0500

    Tabs to spaces

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,36 +1,36 @@
 package com.gildedrose;
 
 class GildedRose {
-	static final String AGED_BRIE = "Aged Brie";
-	static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-	static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-	static final String CONJURED = "Conjured";
-	Item[] items;
-	private ItemUpdaterFactory itemUpdaterFactory = new ItemUpdaterFactory();
+    static final String AGED_BRIE = "Aged Brie";
+    static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    static final String CONJURED = "Conjured";
+    Item[] items;
+    private ItemUpdaterFactory itemUpdaterFactory = new ItemUpdaterFactory();
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
 
-	public void updateAtEndOfDay() {
-		for (Item item : items) {
-			updateQuality(item);
-			updateSellIn(item);
-		}
-	}
+    public void updateAtEndOfDay() {
+        for (Item item : items) {
+            updateQuality(item);
+            updateSellIn(item);
+        }
+    }
 
-	private void updateQuality(Item item) {
-		ItemUpdater itemUpdater = createItemUpdater(item.name);
-		itemUpdater.updateQuality(item);
-	}
+    private void updateQuality(Item item) {
+        ItemUpdater itemUpdater = createItemUpdater(item.name);
+        itemUpdater.updateQuality(item);
+    }
 
-	private ItemUpdater createItemUpdater(String itemName) {
-		return itemUpdaterFactory.createItemUpdater(itemName);
-	}
+    private ItemUpdater createItemUpdater(String itemName) {
+        return itemUpdaterFactory.createItemUpdater(itemName);
+    }
 
-	private void updateSellIn(Item item) {
-		if (!item.name.equals(SULFURAS)) {
-			item.sellIn = item.sellIn - 1;
-		}
-	}
+    private void updateSellIn(Item item) {
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
 }
--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -1,7 +1,7 @@
 package com.gildedrose;
 
 public class ItemUpdaterFactory {
-	public ItemUpdater createItemUpdater(String itemName) {
-		return new DefaultUpdater();
-	}
+    public ItemUpdater createItemUpdater(String itemName) {
+        return new DefaultUpdater();
+    }
 }
