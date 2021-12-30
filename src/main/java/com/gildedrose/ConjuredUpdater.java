6d6f51f96bf2c39a669b8caa062c2e30e1cd035e
Daniel Steinberg <d215steinberg@yahoo.com>
2021-12-30 14:30:23 -0500

    Tabs to spaces

--- src/main/java/com/gildedrose/ConjuredUpdater.java
+++ src/main/java/com/gildedrose/ConjuredUpdater.java
@@ -2,13 +2,13 @@ package com.gildedrose;
 
 public class ConjuredUpdater extends DefaultUpdater {
 
-	@Override
-	public void updateQuality(Item item) {
-		if (item.sellIn > 0) {
-			item.quality -= 2;
-		} else {
-			item.quality -= 4;
-		}
-	}
+    @Override
+    public void updateQuality(Item item) {
+        if (item.sellIn > 0) {
+            item.quality -= 2;
+        } else {
+            item.quality -= 4;
+        }
+    }
 
 }
