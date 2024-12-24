c0d61ceec629bb04ac673645e5b2a59ff5467bb7
d215steinberg <d215steinberg@yahoo.com>
2024-12-23 19:34:46 -0500

    Restored changes

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import static com.gildedrose.GildedRose.CONJURED;
-
 public class ItemUpdaterFactory {
     public ItemUpdater createItemUpdater(String itemName) {
         return ItemType.forName(itemName).createItemUpdater();
