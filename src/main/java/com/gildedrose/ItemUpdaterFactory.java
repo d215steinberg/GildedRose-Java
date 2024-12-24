4d32fbe22f555eeab1b343abb47204a52ae4c536
d215steinberg <d215steinberg@yahoo.com>
2024-12-23 19:28:35 -0500

    Restored change to ItemUpdaterFactory

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -4,11 +4,6 @@ import static com.gildedrose.GildedRose.CONJURED;
 
 public class ItemUpdaterFactory {
     public ItemUpdater createItemUpdater(String itemName) {
-        switch (itemName) {
-            case CONJURED:
-                return new ConjuredUpdater();
-            default:
-                return new DefaultUpdater();
-        }
+        return ItemType.forName(itemName).createItemUpdater();
     }
 }
